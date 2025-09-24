15cd5f436a39d3fa1c96e1cd894e3c528960a591
Felix Grzelka <felix.grzelka@dataport.de>
2025-09-24 11:11:27 +0000

    only check file for now

--- test/zsh-history/test.sh
+++ test/zsh-history/test.sh
@@ -19,9 +19,9 @@ check "validate zsh history HIST_IGNORE_ALL_DUPS" cat /root/.zshrc | grep 'setop
 check "validate zsh history INC_APPEND_HISTORY" cat /root/.zshrc | grep 'setopt INC_APPEND_HISTORY'
 check "validate zsh history HISTTIMEFORMAT" cat /root/.zshrc | grep 'HISTTIMEFORMAT=%F, %T '
 
-check "validate zsh history env var HISTFILE" zsh -c 'echo $HISTFILE'
-# | grep '/root/.history/zsh_history'
-check "validate zsh history HISTSIZE" zsh -c 'echo $HISTSIZE'
+# check "validate zsh history env var HISTFILE" zsh -c 'echo $HISTFILE'
+# # | grep '/root/.history/zsh_history'
+# check "validate zsh history HISTSIZE" zsh -c 'echo $HISTSIZE'
 # | grep '1000000000'
 # check "validate zsh history HISTFILESIZE" cat /root/.zshrc | grep 'HISTFILESIZE=1000000000'
 # check "validate zsh history HIST_IGNORE_ALL_DUPS" cat /root/.zshrc | grep 'setopt HIST_IGNORE_ALL_DUPS'
